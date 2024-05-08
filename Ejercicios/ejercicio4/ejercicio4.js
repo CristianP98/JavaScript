1158a289b46cf39ecbf1fa7e52e4c37d217f3e0f
CristianP98 <Cristian.panche12@gmail.com>
2024-05-08 19:21:49 +0200

    Ejercicios

--- Ejercicios/ejercicio4/ejercicio4.js
+++ Ejercicios/ejercicio4/ejercicio4.js
@@ -2,14 +2,21 @@
 En este ejercicio vas a pedir al usuario que teclee tres números enteros y el script mostrará como resultado el valor medio de los tres.
 Recuerda que la media de tres números se calcula sumando los tres y dividiendo entre 3.
 
-input: Numero (1,2,3)
-outpust: media entre 3
+inputs: Numero (1,2,3) // enteros
+outpust: media entre 3 (promedios) // floats
+
+ejemplos: 
+
+inputs: 10, 20, 30
+outputs: 60/3 = 20
 */
 
-let num1 = prompt("Escribe el primer número", 0);
-let num2 = prompt("Escribe el segundo número", 0);
-let num3 = prompt("Escribe el tercer número", 0);
+// Pedimos los tres números (Se pone el Number para que los datos sean numericos)
+let num1 = Number(prompt("Escribe el primer número"));
+let num2 = Number(prompt("Escribe el segundo número"));
+let num3 = Number(prompt("Escribe el tercer número"));
 
-let total = (num1 + num2 + num3)/3;
+// promedio
+let promedio = (num1 + num2 + num3) / 3;
 
-alert("El valor total es: " + total);
+document.write ("El promedio es: " + promedio);
