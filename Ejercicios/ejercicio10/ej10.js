fed826f6b88f54399cad4ff5640ed128a8cfe03f
CristianP98 <Cristian.panche12@gmail.com>
2024-05-09 19:08:25 +0200

    ejercicio10

---
+++ Ejercicios/ejercicio10/ej10.js
@@ -0,0 +1,9 @@
+/* 
+Escribe un script  que dado un número nos devuelva true si es un número par y false si es un número impar. No usar condiconales.
+
+Ejemplo: 
+numero: 50
+
+¿Es par? : true
+
+*/
