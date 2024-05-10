9127b3133e043ba036309743423be6cdd0b3de83
CristianP98 <Cristian.panche12@gmail.com>
2024-05-10 15:56:36 +0200

    ejercicio 10

--- Ejercicios/ejercicio10/ej10.js
+++ Ejercicios/ejercicio10/ej10.js
@@ -1,5 +1,6 @@
 /* 
-Escribe un script  que dado un número nos devuelva true si es un número par y false si es un número impar. No usar condiconales.
+Escribe un script  que dado un número nos devuelva true si es un número par y false si es un número impar.
+No usar condiconales.
 
 Ejemplo: 
 numero: 50
@@ -7,3 +8,13 @@ numero: 50
 ¿Es par? : true
 
 */
+
+// El usuario ponga el numero
+let numero = Number.parseInt(prompt("Ingrese un número"));
+
+// operacion
+let resto = (numero %2);
+let numPar = (resto == 0);
+console.log("El numero par es: " + numPar);
+
+document.getElementById("clase").innerText = `El numero ${numero} es ${numPar};
