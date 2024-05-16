a9a2b620fee15bb6ecfa5824f82a3509d7e61fc7
CristianP98 <Cristian.panche12@gmail.com>
2024-05-16 17:08:03 +0200

    cambios

--- operadores/ejercicios/ejercicio1/ejercicio.js
+++ operadores/ejercicios/ejercicio1/ejercicio.js
@@ -13,6 +13,7 @@ let a = 10;
 let b = 5;
 let c = 2;
 let total = (a + b * c);
+
 document.getElementById("total-1").innerHTML= `º El resultado de la operación 10 + 5 * 2 es: ${total}`;
 
 let total2 = ((a + b)* c);
@@ -27,11 +28,11 @@ Calcular el radio
 Área del círculo: 3.14 * r ** 2, con r igual a 10 -> ¿cómo podemos hacer un programa que reciba el radio r como dato y devuelva el área de la circunferencia?
 */
 // respuesta del ejercicio 2
-/* let PI = 3.14 * 100;
+let PI = 3.14 * 100;
 let radio = Number.parseInt(prompt('Introduce el numero de radio'));
 let circunferencia = (PI * radio ** 2);
 // Respuesta en pantalla
-document.getElementById("total-4").innerHTML= `La circunferencia del número dado es: ${circunferencia}`; */
+document.getElementById("total-4").innerHTML= `La circunferencia del número dado es: ${circunferencia}`;
 
 
 /* Elevaciones
@@ -49,10 +50,10 @@ Calcular Area
 Área del triángulo: Encuentra el área de un triángulo con base 6 y altura 4. -> ¿Cómo harías una calculadora de áreas de triángulos?
 */
 
-/* let altura = Number.parseInt(prompt('Ingrese el numero de la altura'));
+let altura = Number.parseInt(prompt('Ingrese el numero de la altura'));
 let base = Number.parseInt(prompt('Ingrese el numero de la base'));
-let totalTriangulo = (altura * base / 2);
-document.getElementById("areaDeTriangulos").innerHTML = `El area del triangulo es: ${totalTriangulo}`; */
+let totalTriangulo = (base * altura / 2);
+document.getElementById("areaDeTriangulos").innerHTML = `El area del triangulo es: ${totalTriangulo}`;
 
 
 /* 
@@ -63,7 +64,7 @@ Resultado de la operacion 25 / (5 - 5)
 
 let operacion = 25 / (5 - 5) + 10;
 console.log(operacion);
-// El resultado es infinito y la solucion seria: 
+// El resultado es infinito (la operación no se puede hacer porque no se puede dividir por 0) y la solucion seria: 
 
 let operacion2 = (25 / 5 - 5) + 10;
 console.log(operacion2);
@@ -78,3 +79,7 @@ let totalLongitud = (longitud * 4);
 let totalArea = (longitud * longitud);
 let perimetro = (longitud / 4); 
 console.log(totalArea, totalLongitud, perimetro);
+
+
+let valor1 = 5 + 10 * 2;
+console.log(valor1);
