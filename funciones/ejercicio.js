79abe4e7b32add9e2034cbe48422e12e3e4d4450
CristianP98 <Cristian.panche12@gmail.com>
2024-05-21 19:00:47 +0200

    Ejercicios

--- funciones/ejercicio.js
+++ funciones/ejercicio.js
@@ -5,9 +5,10 @@ Crea una función que tenga dos argumentos: num1, num2, y que devuelva como resu
 function suma(num1, num2){ 
     return num1 + num2;
 }
-suma( 3, 5);
+suma(3, 5);
 
 console.log('El resultado de la suma es: ',suma( 3, 5));
+
 
 /*
 Esta función se va a llamar esPar(num) y deberá devolver verdadero si el argumento es par y falso si es impar. Se acepta que solo se usarán números válidos.
@@ -23,5 +24,21 @@ const esPar = function(num){
  Función que calcule si un número es múltiplo de otro. La función recibirá dos argumentos, el primero será el supuesto mutiplo del segundo argumento.
  */
 
+ const multiplo = function (mult,numero){
+    return (numero % mult == 0);
+ }
+ let dato = Number.parseInt(prompt('Ingrese un número: '));
+ let dato2 = Number.parseInt(prompt('Ingrese un número: '));
+ console.log(multiplo(dato, dato2));
 
- 
+/* 
+Definir una función que cree una cadena de letras repetidas tantas veces como le digamos. La función recibe dos argumentos: la letra y el número de repeticiones.
+*/
+
+const repetir = function (letra, numero){
+    return letra.repeat(numero);
+}
+let letras = prompt('Ingrese una letra: ');
+let ingreseNumero = prompt('Ingrese un número: ');
+
+console.log(repetir(letras, ingreseNumero));
