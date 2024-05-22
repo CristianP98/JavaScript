c8f1501f30d36d706c691ae96b88e3f47611aa74
CristianP98 <Cristian.panche12@gmail.com>
2024-05-22 15:55:24 +0200

    Terminado

--- Proyectos Ramdon/Cronometro/codigo.js
+++ Proyectos Ramdon/Cronometro/codigo.js
@@ -4,25 +4,53 @@ let timerRef = document.querySelector('.timerDisplay');
 let int = null;
 
 // Ejecución del boton de inicio (start)
-document.getElementById('startTimer').addEventListener('click', () => {
-    if(int !==null){
+document.getElementById('startTimer').addEventListener('click', ()=>{
+    if(int!==null){
         clearInterval(int);
     }
     int = setInterval(displayTimer,10);
 });
 
 // Implementación del boton de pausa (pause)
-document.getElementById('pauseTimer').addEventListener('click', () => {
+document.getElementById('pauseTimer').addEventListener('click', ()=>{
     clearInterval(int);
 });
 
 // Implementación del boton de reiniciar (reset)
-document.getElementById('resetTimer').addEventListener('click',() =>{
+document.getElementById('resetTimer').addEventListener('click', ()=>{
     clearInterval(int);
-    [milliseconds, seconds, minutes, hours] = [0,0,0,0];
-    timerRef.innerHTML = '00:00:00:00';
-})
+    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+});
 
 // Funcion del cronometro
 
-https://dev.to/shantanu_jana/create-a-simple-stopwatch-using-javascript-3eoo
+function displayTimer(){
+    milliseconds+=10;
+
+    if(milliseconds == 1000){
+        milliseconds = 0;
+        seconds++;
+
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+}
+
+/* agregué un cero al caso de una cantidad de veces. Cuando el valor del tiempo de cuenta atrás es menor que diez, sumamos un cero. */
+
+let h = hours < 10 ? "0" + hours : hours;
+let m = minutes < 10 ? "0" + minutes : minutes;
+let s = seconds < 10 ? "0" + seconds : seconds;
+let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+
+// ordenar infomración en pantalla
+
+timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
