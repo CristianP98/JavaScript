63d32b80d959d31ef10d8b6be9f717fca402eabb
CristianP98 <Cristian.panche12@gmail.com>
2024-05-22 18:07:14 +0200

    conometro terminado

--- Proyectos Ramdon/Cronometro/codigo.js
+++ Proyectos Ramdon/Cronometro/codigo.js
@@ -1,56 +1,67 @@
-// Acttivacion del cronometro
-let [milliseconds, seconds, minutes, hours] = [0,0,0,0];
-let timerRef = document.querySelector('.timerDisplay');
-let int = null;
+// Se declaran las variables
+let centesimas = 0;
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
 
-// Ejecución del boton de inicio (start)
-document.getElementById('startTimer').addEventListener('click', ()=>{
-    if(int!==null){
-        clearInterval(int);
-    }
-    int = setInterval(displayTimer,10);
-});
-
-// Implementación del boton de pausa (pause)
-document.getElementById('pauseTimer').addEventListener('click', ()=>{
-    clearInterval(int);
-});
-
-// Implementación del boton de reiniciar (reset)
-document.getElementById('resetTimer').addEventListener('click', ()=>{
-    clearInterval(int);
-    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
-});
-
-// Funcion del cronometro
-
-function displayTimer(){
-    milliseconds+=10;
-
-    if(milliseconds == 1000){
-        milliseconds = 0;
-        seconds++;
-
-        if(seconds == 60){
-            seconds = 0;
-            minutes++;
-
-            if(minutes == 60){
-                minutes = 0;
-                hours++;
-            }
-        }
-    }
+// se crean las funciones para el inicio, parar, reinicio
+function inicio () {
+	control = setInterval(cronometro,10);
+	document.getElementById("inicio").disabled = true;
+	document.getElementById("parar").disabled = false;
+	document.getElementById("continuar").disabled = true;
+	document.getElementById("reinicio").disabled = false;
+}
+function parar () {
+	clearInterval(control);
+	document.getElementById("parar").disabled = true;
+	document.getElementById("continuar").disabled = false;
+}
+function reinicio () {
+	clearInterval(control);
+	centesimas = 0;
+	segundos = 0;
+	minutos = 0;
+	horas = 0;
+	Centesimas.innerHTML = ":00";
+	Segundos.innerHTML = ":00";
+	Minutos.innerHTML = ":00";
+	Horas.innerHTML = "00";
+	document.getElementById("inicio").disabled = false;
+	document.getElementById("parar").disabled = true;
+	document.getElementById("continuar").disabled = true;
+	document.getElementById("reinicio").disabled = true;
 }
 
-/* agregué un cero al caso de una cantidad de veces. Cuando el valor del tiempo de cuenta atrás es menor que diez, sumamos un cero. */
-
-let h = hours < 10 ? "0" + hours : hours;
-let m = minutes < 10 ? "0" + minutes : minutes;
-let s = seconds < 10 ? "0" + seconds : seconds;
-let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
-
-// ordenar infomración en pantalla
-
-timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+// la funciond el cronometro
+function cronometro () {
+	if (centesimas < 99) {
+		centesimas++;
+		if (centesimas < 10) { centesimas = "0"+centesimas }
+		Centesimas.innerHTML = ":"+centesimas;
+	}
+	if (centesimas == 99) {
+		centesimas = -1;
+	}
+	if (centesimas == 0) {
+		segundos ++;
+		if (segundos < 10) { segundos = "0"+segundos }
+		Segundos.innerHTML = ":"+segundos;
+	}
+	if (segundos == 59) {
+		segundos = -1;
+	}
+	if ( (centesimas == 0)&&(segundos == 0) ) {
+		minutos++;
+		if (minutos < 10) { minutos = "0"+minutos }
+		Minutos.innerHTML = ":"+minutos;
+	}
+	if (minutos == 59) {
+		minutos = -1;
+	}
+	if ( (centesimas == 0)&&(segundos == 0)&&(minutos == 0) ) {
+		horas ++;
+		if (horas < 10) { horas = "0"+horas }
+		Horas.innerHTML = horas;
+	}
+}
